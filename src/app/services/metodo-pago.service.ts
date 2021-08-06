004f805a9a1eb71d0da70a0d684cb463923489a8
SantiVelez1012 <santivekez1012@gmail.com>
2021-08-06 12:37:21 -0500

    servicios terminados

--- src/app/services/metodo-pago.service.ts
+++ src/app/services/metodo-pago.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MetodoPago } from '../model/metodo-pago';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MetodoPagoService {
 
-  constructor() { }
+  private baseEndPoint = 'http://localhost:8080/metodopago';
+  constructor(private http : HttpClient) { }
+
+  public ver(id: number): Observable<MetodoPago> {
+    return this.http.get<MetodoPago>(`${this.baseEndPoint}/find/${id}`)
+  }
+
 }
